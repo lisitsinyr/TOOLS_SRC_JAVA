fab523464c0ae820e1654854e89f6dbee3bdac46
lisitsinyr <lisitsinyr@gmail.com>
2024-09-13 11:24:19 +0400

    20240913112418

--- src/Version/src/Version_main_JAVA.bat
+++ src/Version/src/Version_main_JAVA.bat
@@ -15,6 +15,6 @@ set P1=C:\Program Files\Far Manager\Far.exe
 rem -------------------------------------------------------------------
 
 :Begin
-"C:\Program Files\Java\jdk-19\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA 2022.2\lib\idea_rt.jar=62802:C:\Program Files\JetBrains\IntelliJ IDEA 2022.2\bin" -Dfile.encoding=windows-1251 -classpath D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\03_Java\PROJECTS_JAVA\TOOLS_JAVA\out\production\TOOLS_JAVA;D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\03_Java\PROJECTS_JAVA\TOOLS_JAVA\out\production\Version;D:\PROJECTS_LYR\CHECK_LIST\05_DESKTOP\03_Java\PROJECTS_JAVA\LIB\jcommander\jcommander-1.82.jar Version -P1 "C:\Program Files\Far Manager\Far.exe"
+"C:\Program Files\Java\jdk-19\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA 2022.2\lib\idea_rt.jar=62802:C:\Program Files\JetBrains\IntelliJ IDEA 2022.2\bin" -Dfile.encoding=windows-1251 -classpath D:\PROJECTS_LYR\CHECK_LIST\DESKTOP\03_Java\PROJECTS_JAVA\TOOLS_JAVA\out\production\TOOLS_JAVA;D:\PROJECTS_LYR\CHECK_LIST\DESKTOP\03_Java\PROJECTS_JAVA\TOOLS_JAVA\out\production\Version;D:\PROJECTS_LYR\CHECK_LIST\DESKTOP\03_Java\PROJECTS_JAVA\LIB\jcommander\jcommander-1.82.jar Version -P1 "C:\Program Files\Far Manager\Far.exe"
 
 :Exit
